1ee14137fbf6014fd76760322002df45324005b4
이용만 <dydaks7878@gmail.com>
2023-10-06 10:24:37 +0900

    Chart

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import { useQuery } from 'react-query';
+import { fetchCoinHistory } from '../api';
+import ApexChart from "react-apexcharts"
+
+interface CharProps {
+  coinId: string;
+}
+
+interface IHisorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
+function Chart({coinId} : CharProps) {
+  const {isLoading, data} = useQuery<IHisorical[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId),
+  {
+    refetchInterval: 10000,
+  }
+
+  );
+  return (
+    <>
+      {isLoading ? "Loading chart..." : 
+        <ApexChart 
+          type='line' 
+          series={[
+            {
+              name: "Price",
+              data: data?.map(price => parseFloat(price.close)) ?? [] // ?? [] : 데이터가 null 되는걸 방지
+            },
+          ]}
+          options={{
+            theme:{
+              mode: "dark"
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show:false
+              },
+              background: "transparent",
+            },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            grid: {
+              show:false,
+            },
+            xaxis: {
+              labels:{
+                show:false
+              },
+              axisTicks:{
+                show:false
+              },
+              axisBorder:{
+                show:false
+              },
+              categories: data?.map((price) => price.time_open),
+              type: "datetime"
+            },
+            yaxis: {
+              show: false
+            },
+            fill:{
+              type: "gradient",
+              gradient: {gradientToColors: ["blue"], stops: [0, 100]},
+            },
+            colors: ["red"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$ ${value.toFixed(2)}`,
+              }
+            }
+          }} 
+        />
+      }
+    </>
+  )
+}
+
+export default Chart
---
+++ src/routes/Coins.tsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { fetchCoins } from '../api';
+import {Helmet} from "react-helmet";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${props => props.theme.bgColor};
+  margin-bottom: 10px;
+  border-radius: 10px;
+  &:hover {
+    a{
+      color: ${(props) => props.theme.accentColor}
+    }
+  }
+  a {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    transition: color 0.2s ease-in;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${props => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`
+
+const Img = styled.img`
+  width: 25px;
+  height: 25px;
+  margin-right: 10px;
+`
+
+interface CoinInterface {
+  id: string,
+  name: string,
+  symbol: string,
+  rank: number,
+  is_new: boolean,
+  is_active: boolean,
+  type: string,
+}
+
+function Coins() {
+  const {isLoading, data } = useQuery<CoinInterface[]>("allCoins", fetchCoins)
+  // const [coins,setCoins] = useState<CoinInterface[]>([]);
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   (async() => {
+  //     setCoins(json.slice(0,100));
+  //     setLoading(false);
+  //   })();
+  // }, []);
+  return (
+    <Container>
+      <Helmet>
+        <title>
+          코인
+        </title>
+      </Helmet>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      {isLoading ? (<Loader>Loading...</Loader>) : (
+      <CoinList>
+        {data?.slice(0,100).map((coin) => <Coin key={coin.id}>
+          <Link to={{
+            pathname: `/${coin.id}`,
+            state: { name: coin.name },
+          }}>
+              <Img src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`} />
+              {coin.name} &rarr;
+          </Link>
+        </Coin>)}
+      </CoinList>)}
+    </Container>
+  )
+}
+
+export default Coins

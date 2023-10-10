dc97c5fd34e780e8ccefe225d662bb3532503218
이용만 <dydaks7878@gmail.com>
2023-10-10 21:53:23 +0900

    Recoil 적용(2)

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { fetchCoins } from '../api';
 import {Helmet} from "react-helmet";
+import { useSetRecoilState } from 'recoil';
+import { isDarkAtom } from '../atoms';
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -66,15 +68,9 @@ interface CoinInterface {
 
 
 function Coins() {
+  const setDarkAtom = useSetRecoilState(isDarkAtom);
+  const toggleDarkAtom = () => setDarkAtom((prev) => !prev);
   const {isLoading, data } = useQuery<CoinInterface[]>("allCoins", fetchCoins)
-  // const [coins,setCoins] = useState<CoinInterface[]>([]);
-  // const [loading, setLoading] = useState(true);
-  // useEffect(() => {
-  //   (async() => {
-  //     setCoins(json.slice(0,100));
-  //     setLoading(false);
-  //   })();
-  // }, []);
   return (
     <Container>
       <Helmet>
@@ -84,7 +80,7 @@ function Coins() {
       </Helmet>
       <Header>
         <Title>Coins</Title>
-        
+        <button onClick={toggleDarkAtom}>ToggleButton</button>
       </Header>
       {isLoading ? (<Loader>Loading...</Loader>) : (
       <CoinList>
